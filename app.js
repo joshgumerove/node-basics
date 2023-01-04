0d4e26d0b08679f425e1d75898214aedd749c9b2
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-04 14:19:52 -0500

    understanding requests

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const http = require("http"); // how we import files in nodejs
 
 const server = http.createServer((req, res) => {
-  console.log("running on port 3000");
+  console.log(req); // generated when we visit localhost:3200 for us by node
+  console.log("running on port 3200");
+  // console.log("data: ", req.url, req.method, req.headers); logging ceratin request data
+  // process.exit(); if we want to quit process
 });
 
-server.listen(3000);
+server.listen(3200); // server should stay up and running
 
 // note the core modules that node ships with:
 // fs
