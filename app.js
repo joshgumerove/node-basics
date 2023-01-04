aea6b6ad411c8eb3a369032de6707eb24fa960e6
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-04 16:58:28 -0500

    send post request

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const http = require("http"); // how we import files in nodejs
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // console.log(req); // generated when we visit localhost:3200 for us by node
   console.log("running on port 3200");
+
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>"); // allows us to write data to the
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text'/><button type='submit'>Submit</button></form></body>"
+    );
+    res.write("</htm>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY TEXT");
+    res.statusCode = 302;
+    res.setHeader = ("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/htMl"); // tell the browser that we are rendering HTML code
   res.write("<html>"); // allows us to write data to the
   res.write("<head><title>My First Page</title></head>");
