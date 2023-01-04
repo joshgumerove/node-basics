678f18c7580547a077108763a08a29d0441b17b1
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-04 14:31:30 -0500

    sending a response

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const http = require("http"); // how we import files in nodejs
 
 const server = http.createServer((req, res) => {
-  console.log(req); // generated when we visit localhost:3200 for us by node
+  // console.log(req); // generated when we visit localhost:3200 for us by node
   console.log("running on port 3200");
-  // console.log("data: ", req.url, req.method, req.headers); logging ceratin request data
+  res.setHeader("Content-Type", "text/html"); // tell the browser that we are rendering HTML code
+  res.write("<html>"); // allows us to write data to the
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from Node Server</h1></body>");
+  res.write("</htm>");
+  res.end();
+  // console.log("data: ", req.url, req.method, req.headers); logging ceratin request  (note that GET is the default if not specifid in header)
   // process.exit(); if we want to quit process
 });
 
