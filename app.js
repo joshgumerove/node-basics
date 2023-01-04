1a07ab64f5c74a7072d27766e14885152dfef9cf
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-04 16:29:08 -0500

    notes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require("http"); // how we import files in nodejs
 const server = http.createServer((req, res) => {
   // console.log(req); // generated when we visit localhost:3200 for us by node
   console.log("running on port 3200");
-  res.setHeader("Content-Type", "text/html"); // tell the browser that we are rendering HTML code
+  res.setHeader("Content-Type", "text/htMl"); // tell the browser that we are rendering HTML code
   res.write("<html>"); // allows us to write data to the
   res.write("<head><title>My First Page</title></head>");
   res.write("<body><h1>Hello from Node Server</h1></body>");
